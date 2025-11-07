e5e3c4defb0042dce9a6d6ff1ea7301da7ae1c19
Klayton Rodrigues De Sousa <147443157+Klayton2@users.noreply.github.com>
2025-11-07 07:30:59 -0300

    07/11
    adicionei alguns alertas na pagina de formulario

--- portifolio/script.js
+++ portifolio/script.js
@@ -10,3 +10,33 @@ document.querySelectorAll('nav a').forEach(link =>{
         }    
     });
 });
+
+const form = document.getElementById('formCont');
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const number = document.getElementById('number').value.trim();
+    const txtarea = document.getElementById('txtarea').value.trim();
+
+    if(name == ""){
+        alert('Por favor preencha o campo Nome !');
+        return;
+    }
+
+    if(email == ""){
+        alert('Por favor preencha o campo Email !');
+        return;
+    }
+
+    if(txtarea == ""){
+        alert('Por favor preencha o campo Mensagem !');
+        return;
+    }
+
+    alert('Mensagem enviada com sucesso !');
+    form.reset();
+});
+
